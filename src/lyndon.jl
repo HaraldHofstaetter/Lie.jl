a335d2f96c33c0116b38ba8ff127d4ef37d78400
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2019-12-03 22:47:08 +0100

    improvements lookup-table, parallelization

--- src/lyndon.jl
+++ src/lyndon.jl
@@ -123,32 +123,36 @@ function hom_class(K::Int, w::Vector{Int}, l::Int, r::Int)
 end
 
 
-#function word_to_index(K::Int, w::Vector{Int}, l::Int, r::Int)
-#    x = w[r]
-#    y = K
-#    for j=r-1:-1:l
-#        x += w[j]*y
-#        y *= K
-#    end
-#    x
-#end
+function word_to_index(K::Int, w::Vector{Int}, l::Int, r::Int)
+    x = w[r]
+    y = K
+    for j=r-1:-1:l
+        x += w[j]*y
+        y *= K
+    end
+    x + div(y-1, K-1)
+end
 
 
 function coeff(K::Int, w::Vector{Int}, l::Int, r::Int, j::Int, 
                p1::Vector{Int}, p2::Vector{Int}, 
                nn::Vector{Int}, h::Vector{Vector{Int}},
-               H::Matrix{Vector{Int}})
-  #             HT::Dict{Tuple{Int, Int, Int}, Int}, M::Int)
+               H::Matrix{Vector{Int}},
+               W2I::Matrix{Int},
+               #HT::Dict{Tuple{Int, Int}, Int},
+               HT::Matrix{Int},
+               M::Int)
     if l==r 
         return @inbounds w[l]==j-1 ? 1 : 0
     end
 
- #   if r-l+1<=M # use hash table
- #       return get(HT, (j, r-l+1, word_to_index(K, w, l, r)), 0)  
- #   end
-
     if @inbounds H[l,r]!=h[j]
         return 0
+    end
+
+    if r-l+1<=M # use hash table
+        #return get(HT, (j, W2I[l,r]), 0)  
+        return @inbounds HT[j, W2I[l,r]]
     end
 
 @inbounds j1 =p1[j]
@@ -156,105 +160,31 @@ function coeff(K::Int, w::Vector{Int}, l::Int, r::Int, j::Int,
 @inbounds m1 = nn[j1]
 @inbounds m2 = nn[j2]
 
-    if m1>m2
-        c1 = coeff(K, w, l, l+m1-1, j1, p1, p2, nn, h, H)#, HT, M)
+    if m1<m2
+        c1 = coeff(K, w, l, l+m1-1, j1, p1, p2, nn, h, H, W2I, HT, M)
         if c1!=0
-            c1 *= coeff(K, w, l+m1, r, j2, p1, p2, nn, h, H)#, HT, M)
+            c1 *= coeff(K, w, l+m1, r, j2, p1, p2, nn, h, H, W2I, HT, M)
         end
     
-        c2 = coeff(K, w, l+m2, r,  j1, p1, p2, nn, h, H)#, HT, M)
+        c2 = coeff(K, w, l+m2, r,  j1, p1, p2, nn, h, H, W2I, HT, M)
         if c2!=0
-            c2 *= coeff(K, w, l, l+m2-1, j2, p1, p2, nn, h, H)#, HT, M)
+            c2 *= coeff(K, w, l, l+m2-1, j2, p1, p2, nn, h, H, W2I, HT, M)
         end
     else
-        c1 = coeff(K, w, l+m1, r, j2, p1, p2, nn, h, H)#, HT, M)
+        c1 = coeff(K, w, l+m1, r, j2, p1, p2, nn, h, H, W2I, HT, M)
         if c1!=0
-            c1 *= coeff(K, w, l, l+m1-1, j1, p1, p2, nn, h, H)#, HT, M)
+            c1 *= coeff(K, w, l, l+m1-1, j1, p1, p2, nn, h, H, W2I, HT, M)
         end
     
-        c2 = coeff(K, w, l, l+m2-1, j2, p1, p2, nn, h, H)#, HT, M)
+        c2 = coeff(K, w, l, l+m2-1, j2, p1, p2, nn, h, H, W2I, HT, M)
         if c2!=0
-            c2 *= coeff(K, w, l+m2, r,  j1, p1, p2, nn, h, H)#, HT, M)
+            c2 *= coeff(K, w, l+m2, r,  j1, p1, p2, nn, h, H, W2I, HT, M)
         end
     end
 
     c1 - c2
 end
 
-function lyndon_words_basis_trafo(K::Int, N::Int; verbose::Bool=false, M::Int=5)
-    t0 = time()
-    if verbose
-        print("initializing...")
-    end
-    WW = [[c] for c=0:K-1]
-    p1 = collect(1:K)
-    p2 = zeros(Int, K)
-    nn = ones(Int, K)
-    hh = [hom_class(K, [c], 1, 1) for c=0:K-1]
-    wordindex = Dict{Vector{Int},Int}([[i-1]=>i for i=1:K]...)
-    index = K+1
-    ii = zeros(Int, N)
-    ii[1] = index 
-    for n=2:N
-        W,f = lyndon_words_factored(K, n)
-        for j=1:length(W)
-            w = W[j]
-            s1 = w[1:f[j]-1]
-            s2 = w[f[j]:end]
-            wordindex[w]=index
-            push!(p1, wordindex[s1])
-            push!(p2, wordindex[s2])
-            push!(nn, n)
-            push!(hh, hom_class(K, w, 1, n))
-            index += 1
-        end
-        append!(WW, W)
-        ii[n] = index
-    end
-
-    # generate hash table
-    HT = Dict{Tuple{Int, Int, Int}, Int}()
-    for n=2:M
-        j1 = ii[n-1]
-        j2 = ii[n]-1 
-        for k=0:K^n-1
-            w = [parse(Int, c) for c in string(k, base=K, pad=n)]
-            for j=j1:j2
-                c = coeff(K, w, 1, n, j, p1, p2, nn, hh, HT, n-1)
-                if c!=0
-                    HT[(j, n, k)] = c
-                end
-            end
-        end
-    end
-
-    if verbose
-        println("time=", time()-t0)
-    end
-
-
-    T = [Tuple{Int,Int}[] for i=1:ii[N]]
-    for n=2:N
-        if verbose
-            print("n=$n: ")
-        end
-        j1 = ii[n-1]
-        j2 = ii[n]-1 
-        Threads.@threads for j=j1:j2
-            for i=j+1:j2
-                c = coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, HT, M)
-                if c!=0
-                    push!(T[i], (j,c))
-                end
-            end
-        end
-        if verbose
-            println("time=", time()-t0)
-        end
-    end
-
-    WW, p1, p2, nn, T
-end
 
 
 function lyndon_basis_coeffs(G::Vector{Generator}, S::Element, N::Int; 
@@ -292,49 +222,58 @@ function lyndon_basis_coeffs(G::Vector{Generator}, S::Element, N::Int;
         ii[n] = index
     end
 
-#    # generate hash table
-#    HT = Dict{Tuple{Int, Int, Int}, Int}()
-#    for n=2:M
-#        j1 = ii[n-1]
-#        j2 = ii[n]-1 
-#        for k=0:K^n-1
-#            w = [parse(Int, c) for c in string(k, base=K, pad=n)]
-#            for j=j1:j2
-#                c = coeff(K, w, 1, n, j, p1, p2, nn, hh, HT, n-1)
-#                if c!=0
-#                    HT[(j, n, k)] = c
-#                end
-#            end
-#        end
-#    end
+    # generate hash table
+    #HT = Dict{Tuple{Int, Int}, Int}()
+    #for n=1:M
+    #    j1 = n==1 ? 1 : ii[n-1]
+    #    j2 = ii[n]-1 
+    #    for k=0:K^n-1
+    #        w = [parse(Int, c) for c in string(k, base=K, pad=n)]
+    #        H = Vector{Int}[l<=r ? hom_class(K, w, l,r) : [0,0] for l=1:n, r=1:n]
+    #        M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, w, l,r) : 0
+    #                                             for l=1:n, r=1:n]
+    #        wi = word_to_index(K, w, 1, n)
+    #        for j=j1:j2
+    #            c = coeff(K, w, 1, n, j, p1, p2, nn, hh, H, M2I, HT, n-1)
+    #            if c!=0
+    #                HT[(j, wi)] = c
+    #            end
+    #        end
+    #    end
+    #end
+
+    HT = zeros(Int, M==0 ? 0 : ii[M]-1, div(K^(M+1)-1, K-1)-1)
+    for n=1:M
+        j1 = n==1 ? 1 : ii[n-1]
+        j2 = ii[n]-1 
+        for k=0:K^n-1
+            w = [parse(Int, c) for c in string(k, base=K, pad=n)]
+            H = Vector{Int}[l<=r ? hom_class(K, w, l,r) : [0,0] for l=1:n, r=1:n]
+            M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, w, l,r) : 0
+                                                 for l=1:n, r=1:n]
+            wi = word_to_index(K, w, 1, n)
+            for j=j1:j2
+                c = coeff(K, w, 1, n, j, p1, p2, nn, hh, H, M2I, HT, n-1)
+                if c!=0
+                    HT[j, wi] = c
+                end
+            end
+        end
+    end
+
+    if verbose
+        println("time=", time()-t0)
+        print("coeffs of words...")
+    end
+
+    cc = zeros(T, length(WW))
+    Threads.@threads for i=1:length(WW)
+        cc[i] = wcoeff(Word(G[WW[i] .+ 1]), S, T=T)
+    end
+
     if verbose
         println("time=", time()-t0)
     end
-
-    cc = zeros(T, ii[N]-1)
-
- #   for n=1:N
- #       if verbose
- #           print("n=$n: ")
- #       end
- #       j1 = n==1 ? 1 : ii[n-1]
- #       j2 = ii[n]-1 
- #     # Threads.@threads 
- #       for i=j1:j2
- #         w = Word(G[WW[i] .+ 1])
- #           c = wcoeff(w, S, T=T)
- #           for j=1:i-1
- #               if !iszero(cc[j])
- #                   #c -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, HT, M)*cc[j]
- #                   c -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh)*cc[j]
- #               end
- #           end
- #           cc[i] = c
- #       end
- #       if verbose
- #           println("time=", time()-t0)
- #       end
- #   end
 
     Threads.@threads for h in unique(hh)
        j1 = findfirst(x->x==h, hh)
@@ -344,16 +283,15 @@ function lyndon_basis_coeffs(G::Vector{Generator}, S::Element, N::Int;
             n = nn[i]
             w = Word(G[WW[i] .+ 1])
             H = Vector{Int}[l<=r ? hom_class(K, WW[i], l,r) : [0,0] for l=1:n, r=1:n]
-            c = wcoeff(w, S, T=T)
+            M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, WW[i], l,r) : 0 
+                                                 for l=1:n, r=1:n]
             for j=1:i-1
             if h==hh[j]
                 if !iszero(cc[j])
-                    #c -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, HT, M)*cc[j]
-                    c -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, H)*cc[j]
+                    cc[i] -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, H, M2I, HT, M)*cc[j]
                 end
             end
             end
-            cc[i] = c
         end
         end
     end
