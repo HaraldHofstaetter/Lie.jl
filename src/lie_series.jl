2c99059da53ae0c3c68b22d5bea602bc608495f7
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2019-12-15 09:44:56 +0100

    started LieAlgebra

--- src/lie_series.jl
+++ src/lie_series.jl
@@ -69,18 +69,7 @@ function coeff(K::Int, w::Vector{Int}, l::Int, r::Int, j::Int,
     c1 - c2
 end
 
-
-
-function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int; 
-               T::Type=Rational{Int}, verbose::Bool=false, M::Int=0,
-               lists_output::Bool=false)
-    t0 = time()
-    if verbose
-        print("initializing...")
-    end
-    K = length(G)
-    @assert K>=2 && allunique(G)
-
+function init_lie(K::Int, N::Int, M::Int)
     WW = [[c] for c=0:K-1]
     p1 = collect(1:K)
     p2 = zeros(Int, K)
@@ -111,15 +100,15 @@ function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int;
 
     #CT = Dict{Tuple{Int, Int}, Int}()
     #for n=1:M
-    #    j1 = n==1 ? 1 : ii[n-1]
-    #    j2 = ii[n]-1 
+    #    i1 = n==1 ? 1 : ii[n-1]
+    #    i2 = ii[n]-1 
     #    for k=0:K^n-1
     #        w = [parse(Int, c) for c in string(k, base=K, pad=n)]
     #        H = Vector{Int}[l<=r ? hom_class(K, w, l,r) : [0,0] for l=1:n, r=1:n]
     #        M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, w, l,r) : 0
     #                                             for l=1:n, r=1:n]
     #        wi = word_to_index(K, w, 1, n)
-    #        for j=j1:j2
+    #        for j=i1:i2
     #            c = coeff(K, w, 1, n, j, p1, p2, nn, hh, H, M2I, CT, n-1)
     #            if c!=0
     #                CT[(j, wi)] = c
@@ -130,15 +119,15 @@ function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int;
 
     CT = zeros(Int, M==0 ? 0 : ii[M]-1, div(K^(M+1)-1, K-1)-1)
     for n=1:M
-        j1 = n==1 ? 1 : ii[n-1]
-        j2 = ii[n]-1 
+        i1 = n==1 ? 1 : ii[n-1]
+        i2 = ii[n]-1 
         for k=0:K^n-1
             w = [parse(Int, c) for c in string(k, base=K, pad=n)]
             H = Vector{Int}[l<=r ? hom_class(K, w, l,r) : [0,0] for l=1:n, r=1:n]
             M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, w, l,r) : 0
                                                  for l=1:n, r=1:n]
             wi = word_to_index(K, w, 1, n)
-            for j=j1:j2
+            for j=i1:i2
                 c = coeff(K, w, 1, n, j, p1, p2, nn, hh, H, M2I, CT, n-1)
                 if c!=0
                     CT[j, wi] = c
@@ -146,6 +135,22 @@ function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int;
             end
         end
     end
+    p1, p2, nn, WW, ii, hh, CT
+end
+
+
+
+function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int; 
+               T::Type=Rational{Int}, verbose::Bool=false, M::Int=0,
+               lists_output::Bool=false)
+    t0 = time()
+    if verbose
+        print("initializing...")
+    end
+    K = length(G)
+    @assert K>=2 && allunique(G)
+
+    p1, p2, nn, WW, ii, hh, CT = init_lie(K, N, M)
 
     if verbose
         println("time=", time()-t0)
@@ -163,20 +168,19 @@ function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int;
     end
 
     for n=1:N
-        j1 = n==1 ? 1 : ii[n-1]
-        j2 = ii[n]-1 
-        hu = unique(hh[j1:j2])
+        i1 = n==1 ? 1 : ii[n-1]
+        i2 = ii[n]-1 
+        hu = unique(hh[i1:i2])
         Threads.@threads for h in hu 
-            for i=j1:j2
+            for i=i1:i2
             if h==hh[i]
-                 w = Word(G[WW[i] .+ 1])
                  H = Vector{Int}[l<=r ? hom_class(K, WW[i], l,r) : [0,0] for l=1:n, r=1:n]
-                 M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, WW[i], l,r) : 0 
+                 W2I = Int[l<=r && r-l+1<=M ? word_to_index(K, WW[i], l,r) : 0 
                                                       for l=1:n, r=1:n]
-                 for j=1:i-1
+                 for j=i1:i-1
                  if h==hh[j]
                      if !iszero(cc[j])
-                         cc[i] -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, H, M2I, CT, M)*cc[j]
+                         cc[i] -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, H, W2I, CT, M)*cc[j]
                      end
                  end
                  end
@@ -196,3 +200,76 @@ function lie_series(G::Vector{Generator}, S::AlgebraElement, N::Int;
     end
 end
 
+
+mutable struct LieAlgebra
+    K::Int # number of generators
+    N::Int # maximum order 
+    dim::Int
+    p1::Vector{Int}
+    p2::Vector{Int}
+    nn::Vector{Int}
+    S::Vector{Vector{Tuple{Int,Int,Int}}}
+end
+
+
+
+function LieAlgebra(K::Int, N::Int; M::Int=0)
+    @assert K>=2
+
+    p1, p2, nn, WW, ii, hh, CT = init_lie(K, N, M)
+
+    for i=1:length(WW)
+        println("i=$i ---------------------------------")
+        n = nn[i]
+        h = hh[i]
+        w = WW[i]
+        H = Vector{Int}[l<=r ? hom_class(K, w, l,r) : [0,0] for l=1:n, r=1:n]
+        W2I = Int[l<=r && r-l+1<=M ? word_to_index(K, w, l,r) : 0 for l=1:n, r=1:n]
+        for n1 = 1:div(n,2)
+            n2 = n-n1
+            for j1 = (n1==1 ? 1 : ii[n1-1]) : ii[n1]-1
+            for j2 = (n2==1 ? 1 : ii[n2-1]) : ii[n2]-1
+                if j1<j2 && hh[j1]+hh[j2]==h
+                    c1 = coeff(K, w, 1, n1, j1, p1, p2, nn, hh, H, W2I, CT, M)
+                    if c1!=0
+                        c1 *= coeff(K, w, n1+1, n, j2, p1, p2, nn, hh, H, W2I, CT, M)
+                    end
+    
+                    c2 = coeff(K, w, n2+1, n,  j1, p1, p2, nn, hh, H, W2I, CT, M)
+                    if c2!=0
+                        c2 *= coeff(K, w, 1, n2, j2, p1, p2, nn, hh, H, W2I, CT, M)
+                    end
+                    c = c1 - c2
+                    if c!=0 
+                        print("($j1, $j2)=>$c, ") 
+                    end
+                end
+            end
+            end
+        end
+        println()
+    end
+
+#    for n=1:N
+#        i1 = n==1 ? 1 : ii[n-1]
+#        i2 = ii[n]-1 
+#        hu = unique(hh[i1:i2])
+#        Threads.@threads for h in hu 
+#            for i=i1:i2
+#            if h==hh[i]
+#                 H = Vector{Int}[l<=r ? hom_class(K, WW[i], l,r) : [0,0] for l=1:n, r=1:n]
+#                 M2I = Int[l<=r && r-l+1<=M ? word_to_index(K, WW[i], l,r) : 0 
+#                                                      for l=1:n, r=1:n]
+#                 for j=i1:i-1
+#                 if h==hh[j]
+#                     if !iszero(cc[j])
+#                         cc[i] -= coeff(K, WW[i], 1, n, j, p1, p2, nn, hh, H, M2I, CT, M)*cc[j]
+#                     end
+#                 end
+#                 end
+#             end
+#             end
+#         end
+#    end
+    
+end
